0c564d9f2d30de3d088f6efdf702257462348d96
Satria <yogaadesatria06@gmail.com>
2026-07-06 04:22:06 +0700

    v1.8

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,22 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'cjfpqikdjejtklihaeen.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'i0.wp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i1.wp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i2.wp.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'resepkoki.id',
+      },
     ],
   },
 };
